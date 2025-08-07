b8077222ab5ffa3c972698655015548fc7f13dba
sahithav <ssv5@rice.edu>
2025-08-07 12:46:43 -0700

    params to functions

--- src/pages/operator/tsx/layout_components/Library.tsx
+++ src/pages/operator/tsx/layout_components/Library.tsx
@@ -209,10 +209,9 @@ export const Library = (props: CustomizableComponentProps) => {
                 <div className="library-sections-container">
                     {/* Functions Section */}
                     <div className="library-section">
-                        <h3 className="library-section-title" style={{ fontWeight: "600" }}>Robot Functions</h3>
+                        <h3 className="library-section-title" style={{ fontWeight: "600", color: "#ff8c00" }}>Robot Functions</h3>
                         
                         <div className="library-subsection">
-                            <h4 className="library-subsection-title robot-heading">Robot</h4>
                             <div className="library-text">
                                 <div className="function-group">
                                     <div 
@@ -271,7 +270,7 @@ export const Library = (props: CustomizableComponentProps) => {
                         
                         {!shouldHideHumanAPI() && (
                             <div className="library-subsection">
-                                <h3 className="library-section-title" style={{ fontWeight: "600", marginTop: "24px" }}>Human Functions</h3>
+                                <h3 className="library-section-title" style={{ fontWeight: "600", marginTop: "24px", color: "#28a745" }}>Human Functions</h3>
                                 <div className="library-text">
                                     <div className="function-group">
                                         <div 
