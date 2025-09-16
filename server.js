c5ffb307434576d357007a794a1e013536a60481
sahithav <ssv5@rice.edu>
2025-09-15 19:30:18 -0500

    rosbag directory for rice

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.post('/start_rosbag', (req, res) => {
 
      
     //const outputDir = '/media/hello-robot/HCRLAB/rosbags/latest_' + Date.now();
-    const outputDir = '/home/rosbags/latest_' + Date.now();
+    const outputDir = '/rosbags/latest_' + Date.now();
     rosbagProcess = spawn('ros2', [
         'bag', 'record',
         '-a',
