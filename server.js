98b7869e6abeedbc7afe91aa3097d0e79b931107
sahithav <ssv5@rice.edu>
2025-11-02 14:24:04 -0600

    visual servoing demo

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ app.post('/start_rosbag', (req, res) => {
     }
 
      
-    const outputDir = '/media/hello-robot/HCRLAB/rosbags/latest_' + Date.now();
+    const outputDir = '/hello-robot/rosbags/latest_' + Date.now();
     //const outputDir = '/rosbags/latest_' + Date.now();
     rosbagProcess = spawn('ros2', [
         'bag', 'record',
